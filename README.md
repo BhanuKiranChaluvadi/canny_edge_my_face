c9ce7ad1903127c78ed56b6a73e13b0088f93703
BhanuKiran Chaluvadi <nthdimensionalreality@gmail.com>
2017-07-20 12:31:43 -0600

    Implemented edge detection rosnode
    TODO:
        - Update Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # canny_edge_my_face
 canny edge using ROS
+
+##	Dependency
+
+Clone https://github.com/OTL/cv_camera into your workspace
+
+## Launch
+
+```
+roslaunch canny_edge_my_face edge_detection.launch
+```
---
+++ scripts/canny_edge_detection.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class EdgeDetection(object):
+	
+	def __init__(self):
+
+		rospy.init_node('edge_detection')
+
+		self.bridge = CvBridge()
+
+		rospy.Subscriber('/webcam/image_raw', Image, self.image_callback)
+
+		rospy.spin()
+
+	def image_callback(self, msg):
+
+		try:
+			# Convert your ROS image into OPENCV2
+			cv2_img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+			edges = cv2.Canny(cv2_img,100,200)
+			cv2.imshow('Edges', edges)
+			cv2.waitKey(3)
+		except CVBridgeError, e:
+			print(e) 
+			raise e
+
+
+if __name__ == '__main__':
+
+	detector = EdgeDetection()
